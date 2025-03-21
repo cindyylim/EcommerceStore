a8f868a63634a5f14716bb5d6e058174b8d4be0b
Cindy <cindysy3@gmail.com>
2025-03-21 04:58:48 -0700

    Add authentication and product routes

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    if (!req.cookies.accessToken) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No access token provided" });
+    }
+    try {
+      const decoded = jwt.verify(
+        req.cookies.accessToken,
+        process.env.ACESS_TOKEN_SECRET
+      );
+      const user = await User.findById(decoded.userId);
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - User not found" });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Access token expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("Error in protectRoute middleware", error.message);
+    return res.status(401).json({ message: "Unauthorized - Invalid access token" });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+    if (req.user && req.user.role !== "admin") {
+      return res.status(403).json({ message: "Forbidden - Admin access required" });
+    }
+    next();
+}
---
+++ server/routes/product.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts, getProductsByCategory, toggleFeaturedProduct } from "../controllers/product.controller.js";
+
+const router = express.Router();
+router.get("/", protectRoute, adminRoute, getAllProducts);
+router.get("/featured", getFeaturedProducts);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
+router.get("/recommendations", getRecommendedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct);
+
+export default router;
