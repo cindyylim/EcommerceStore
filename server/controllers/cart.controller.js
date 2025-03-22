6f7a31295daf5f01c65e3f141c6f7acaaa11381f
Cindy <cindysy3@gmail.com>
2025-03-21 20:21:52 -0700

    Added cart routes

---
+++ server/controllers/cart.controller.js
@@ -0,0 +1,77 @@
+import Product from "../models/product.model.js";
+export const addToCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItem.find((item) => item.id === productId);
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      user.cartItems.push({ id: productId, quantity: 1 });
+    }
+    await user.save();
+    res.status(200).json(user.cartItems);
+  } catch (error) {
+    console.log("Error in addToCart controller", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const removeAllFromCart = async (req, res) => {
+  try {
+    const user = req.user;
+    const { productId } = req.body;
+    if (!productId) {
+      user.cartItems = [];
+    } else {
+      user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+    }
+    await user.save();
+    res.status(200).json(user.cartItems);
+  } catch (error) {
+    console.log("Error in removeFromCart controller", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateQuantity = async (req, res) => {
+  try {
+    const { id: productId } = req.params;
+    const { quantity } = req.body;
+    const user = req.user;
+    const existingItem = user.cartItems.find((item) => item.id === productId);
+    if (existingItem) {
+      if (quantity === 0) {
+        user.cartItems = user.cartItems.filter((item) => item.id !== productId);
+      } else {
+        existingItem.quantity = quantity;
+      }
+      await user.save();
+      return res.status(200).json(user.cartItems);
+    } else {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.log("Error in updateQuantity controller", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getCartProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ _id: { $in: req.user.cartItems } });
+    const cartItems = products.map((product) => {
+      const item = req.user.cartItems.find(
+        (item) => item.id.toString() === product._id.toString()
+      );
+      return {
+        ...product.toJSON(),
+        quantity: item.quantity,
+      };
+    });
+    return res.status(200).json(cartItems);
+  } catch (error) {
+    console.log("Error in getCartProducts controller", error.message);
+    return res.status(500).json({ message: error.message });
+  }
+};
